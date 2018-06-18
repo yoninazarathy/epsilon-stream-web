edd422e9292416b2b6e4f07117f57a8f84cacd0e
Yoni Nazarathy <yoninazarathy@gmail.com>
2018-06-18 15:07:01 +1000

    fix search bug

--- src/managers/records-manager.js
+++ src/managers/records-manager.js
@@ -39,7 +39,7 @@ export function localRecord(fromRecord,type){
         case "MathObjectLinks":
             return {
                 type: type,
-                hashTags: ('hashTags' in fi) ? fi.hashTags.value : "NONE", //QQQQ do this type of thing everywhere with an exception
+                hashTags: ('hashTags' in fi) ? fi.hashTags.value.split(',') : "NONE", //QQQQ do this type of thing everywhere with an exception
                 imageKey: fi.imageKey.value,
                 ourTitle: fi.ourTitle.value,
                 ourTitleDetail: fi.ourTitleDetail.value,
@@ -52,7 +52,7 @@ export function localRecord(fromRecord,type){
         case "Video":
             return {
                 type: type,
-                hashTags: fi.hashTags.value,
+                hashTags: fi.hashTags.value.split(','),
                 durationSec: fi.durationSec.value,
                 imageURL: 'https://i.ytimg.com/vi/'+fi.youtubeVideoId.value+'/mqdefault.jpg', //fi.imageURL.value, // QQQQ do this properly plz
                 ourTitle: fi.ourTitle.value,
@@ -82,7 +82,7 @@ export function localRecord(fromRecord,type){
                 provider: fi.provider.value,
                 urlOfItem: fi.urlOfItem.value,
                 featureType: featureType,
-                hashTags: fi.hashTags.value,   
+                hashTags: fi.hashTags.value.split(','),   
                 displaySearchPriority: typeof(fi.displaySearchPriority) !== 'undefined' ?
                                                     fi.displaySearchPriority.value  : -1000,
                 hashTagPriorities: typeof(fi.hashTagPriorities) !== 'undefined' ?
@@ -92,7 +92,7 @@ export function localRecord(fromRecord,type){
             return {
                 type: type,
                 body: fi.body.value,
-                hashTags: fi.hashTags.value,
+                hashTags: fi.hashTags.value.split(','),
                 ourTitle: fi.title.value,   
                 imageURL: typeof(fi.imageURL) !== 'undefined' ?
                                 fi.imageURL.value  : "",                                           
@@ -120,31 +120,31 @@ export function recordsOfHashTag(hashTag){
 
 function videosOfHashTag(hashTag){ 
     let vids = store.getState().database.videos
-    return $.grep(vids, (vid) => {
-        return vid.hashTags.includes(hashTag) //QQQQ bug of form #arc, #arcLength
-    })
+    return vids.filter( (el) => {
+          return el.hashTags.includes(hashTag)
+         });
 }
 
 function featuredURLsOfHashTag(hashTag){
     let furls = store.getState().database.featuredURLs
-    return $.grep(furls, (furl) => {
-        return furl.hashTags.includes(hashTag) //QQQQ bug of form #arc, #arcLength
-    })
+    return furls.filter( (el) => {
+          return el.hashTags.includes(hashTag)
+         });
 }
 
 function mathObjectLinksOfHashTag(hashTag){
     let mols = store.getState().database.mathObjectLinks
-    let mols2 = $.grep(mols,(mol) => {
-        return !mol.avoided.includes("web") //QQQQ
-    })
-    return $.grep(mols2, (mol) => {
-        return mol.hashTags.includes(hashTag) //QQQQ bug of form #arc, #arcLength
-    })
+    //let mols2 = $.grep(mols,(mol) => {
+      //  return !mol.avoided.includes("web") //QQQQ
+    // })
+    return mols.filter( (el) => {
+        return el.hashTags.includes(hashTag)
+       });
 }
 
 export function snippetsOfHashTag(hashTag){ 
     let snips = store.getState().database.snippets
-    return $.grep(snips, (snips) => {
-        return snips.hashTags.includes(hashTag) //QQQQ bug of form #arc, #arcLength
-    })
+    return snips.filter( (el) => {
+        return el.hashTags.includes(hashTag)
+       });
 }
