608a716d24d1a5e5b3282332281feb1ae55afa90
Yoni Nazarathy <yoninazarathy@gmail.com>
2018-09-15 22:30:38 +1000

    web ver

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,7 @@ export default withSiteData(() => (
     <div>
       <h1 style={{ textAlign: 'center' }}>Epsilon Stream Web - Under Development</h1>
         <ul>
-          <li> <a href="https://epsiloncoach.com"> Use V0.5 of Epsilon Stream</a>
+          <li> <a href="https://epsiloncoach.com"> Use V0.5 of Epsilon Stream</a> </li>
           <li><Link to={`/video`}>See all videos</Link> </li>
           <li><Link to={`/snippet`}>See all snippets</Link></li>
           <li><Link to={`/topic`}>See all pregenerated math object thingimagis</Link></li>
