480f89a5992bb3ac7a76d83957e30ae6db5c9f02
Yoni Nazarathy <yoninazarathy@gmail.com>
2018-09-16 18:31:15 +1000

    more cleanup and towards redux

---
+++ src/redux/store.js
@@ -0,0 +1,44 @@
+// import {compose,createStore,applyMiddleware} from 'redux'
+// import reducers from './reducers.js'
+// import logger from 'redux-logger'
+// import thunk from 'redux-thunk'
+// import { persistStore, autoRehydrate } from 'redux-persist'
+// //import createHistory from 'history/createBrowserHistory'
+// import {routerMiddleware } from 'react-router-redux' //QQQQ push
+
+// //export const history = createHistory()
+
+
+// //QQQQ Consider moving this to another module
+// const rehashMiddleWare = store => next => action => {
+//     if(action.type === 'FETCH_MATH_OBJECT_STOP'){
+//         //console.log(action.payload)
+//         next(action)
+//         store.dispatch({type: "REHASH_SEARCH_STRINGS",payload:{}})
+//     }else if(action.type === 'FETCH_SNIPPET_STOP'){
+//         next(action)
+//         store.dispatch({type: "REHASH_SNIPPET_STRINGS",payload:{}})
+//     }else{
+//         next(action);
+//     }
+//   }
+
+
+// //const historyMiddleware = routerMiddleware(history);
+
+// const middleware = applyMiddleware(thunk, logger,rehashMiddleWare)
+
+// export const store = createStore(   reducers,
+//                                     undefined,
+//                                     compose (middleware,
+//                                         autoRehydrate()))
+//                                         persistStore(store)
+
+// store.dispatch({type: "RESET_DATABASE_STORE",payload:{}})
+// store.dispatch({type: "RESET_USER_STORE",payload:{}})
+
+// export function isAndroid() {
+//     return store.getState().user.isAndroid;
+// }
+
+// export default store
