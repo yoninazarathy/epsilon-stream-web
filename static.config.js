c1277e39180ab3bb44b172f3127f8a1fe1bc2edc
Yoni Nazarathy <yoninazarathy@gmail.com>
2018-09-20 07:31:29 +1000

    favicon with base64

--- static.config.js
+++ static.config.js
@@ -147,6 +147,26 @@ export default {
       return (
         <Html>
           <Head>
+            <link href="data:image/x-icon;base64,R0lGODlhPwA9APZxAEE0KU01K0E9MEFFNk1BNkFOPXFnX4g7NKw+Of83G/84Hv86I/87JsRAPdBC
+P/9AIP9JJP9SKf9CO/9DPv9QOKBIRNtDQcRYVPNFRP9EQf9GRv9SUv9dXf9paf90dEGTOkGWQEGY
+RWWdQkGgVlmhU3GgRWWnX0GBZUGaeUGqbUGtcpSqTJStVri0VImAev+AQP+SSdu+XP+kUv+tVvPF
+Yf/IZP/Lbv/Pd0GjgEG0jUG8lHG5gk3Am1nEoWXJqHHNr33RtqCZlJS/h/+AgP+MjP+Xl/+nm8O/
+vP+hoP+iov+0qf+5uYnVvKzQqLfWtf/Sgf/Wi8/Bvv/At//Vsv/gqP/jsf/nu5TZw6DVw6Ddyazi
+0Lfm18PdwcPZ0NvZ1//Fxf/R0f/c3Nvr2v/qxf/uzv/x2M/u5Nvy6//o6P/14v/47Of38vPy8v/z
+8/P48//89fP7+P///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5
+BAEAAHIALAAAAAA/AD0AAAf+gHKCg4SFhoRxiYqLioeOj5CRcoyUlY2SmJmWm5aZnoecoZ2fnqKm
+o6SPp6uoqYissIyur7BmZmexcbOTp3BbQDw6wsI9V2asrqtZwcPNwz/Hp6SnZz7O181Xq6WmZ8zY
+4D9w0pLU3+DgQNuQvefo4FnrjqdM7/bCuKaqpmv39+rkCq2qh64Hkyw/7K2RN2iVu2ZaFq1JCC5i
+QEGrzKDbUskath/IGp7SEs5SP2w8QmI8lUVYDhQwhXG05PGaSlZYThQAwBPAABzRKv1IEeLDBxAj
+VOiAtdJUkgo9owrosmmH0atGQ4wJaapNBw0IokZ1YckN1rM1oLwhJyrMBg3+GhyI7WnAEpezV0vU
+qGEjjT5RbTjAhTuXZ91KTfAaXbGXr5pclDwMhnugMNlKYhR/iNG4xhPIi5ZMhmuhsJdNJvDq7Vyj
+CuhEgkdraBCgJ4EjnMSQwCqCBmu+ayF/kT0Yw4UgUdiIcuNECIsWvxtvhUyEONwkud5Uib4XCujY
+sou83s4dtPUNbV7HucG9DGTrHtTHodL+PfEh8sn/dp/Leof89W3CSyXWaYCGJUtQkEACFEixyRMB
+VtJUZPBVosSCGCZgRCVjcFdDcBIOSEkSBQ5xoCJJKJAhhlMw0iF3nwkoIiNoFAiXB0kQsYEEK2II
+ww1VkFEFhB66JuOMi0jDZuNgPPaYwAse/vZYK5uEseRgEziZgAxRdkYFJ7RUUsSVcDHQIwRdNnYD
+iCGKZMlXV2agIoYPzJAmX34d6WYlXpE5wZx13mnDdHruKaafC0RgZ5o35FlomJWAoWSBGyRRhQ1p
+UsFmm6CIAgYR4MG1wRBLKPKGFUSydgMVU4YSCSthgAEGJ2+UMUYVY5TRqiiYyOcrN75CNk2wsSRD
+LEPDHuvqLkgquwizhjqbCLQCSasLtZ0Si+2r8m2ribDepqIPtYEAADs=" rel="icon" type="image/x-icon" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css" />
           </Head>
           <Body>{children}</Body>
